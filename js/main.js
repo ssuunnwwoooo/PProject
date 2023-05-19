5972bb5f196d3c43b9933626e2076955826e4385
sunwoo <133613205+ssuunnwwoooo@users.noreply.github.com>
2023-05-19 14:49:39 +0900

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('.mainSlide').slick({
-        // arrows: ture,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        pauseOnHover: false,
-    });
-})
